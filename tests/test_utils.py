a4964286a71ee64d66df4451f9b1f195f865a030
Sumin Byeon <suminb@gmail.com>
2018-10-22 14:06:42 +0900

    Write a test case for get_routes_for_station

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from transporter.utils import get_route
+from transporter.utils import get_route, get_routes_for_station
 
 
 def test_get_route():
@@ -10,3 +10,13 @@ def test_get_route():
             isinstance(route_entry['ars_id'], int)
         assert 0.0 <= route_entry['latitude'] < 90.0
         assert 0.0 <= route_entry['longitude'] < 180.0
+
+
+def test_get_routes_for_station(app):
+    stations = get_routes_for_station(25139)
+    assert stations['latitude']
+    assert stations['longitude']
+    assert len(stations['entries']) > 0
+    for entry in stations['entries']:
+        assert entry['route_id']
+        assert entry['route_number']
